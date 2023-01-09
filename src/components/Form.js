4e9a03fb63718ee548502b459d9d95d297cf3e9a
claudiauhr <claudia.c.uhr@gmail.com>
2023-01-09 11:40:59 -0500

    finishing form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,33 @@
-import React from 'react'
-
+// Import the useState hook from react
+import { useState } from "react";
+// Always make sure to declare the props parameter so you can use props in your Component
 const Form = (props) => {
+    // State to hold the data of our form
+    const [formState, setFormState] = useState({
+        searchterm: "",
+    });
+    const handleChange = (e) => {
+        // Use the event (e) object to update value
+        setFormState({ searchterm: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        //Prevent page from refreshing on form submission
+        e.preventDefault();
+        //pass the search term to the getMovie prop
+        props.getMovie(formState.searchterm);
+    };
+
+    // The Component must return some JSX
   return (
     <div>
-        <form>
-            <input type="text" />
+        <form onSubmit={handleSubmit}>
+            <input 
+            type="text"
+            name="searchterm"
+            onChange={handleChange}
+            value={formState.searchterm} 
+            />
             <input type="submit" value="submit" />
         </form>
     </div>
